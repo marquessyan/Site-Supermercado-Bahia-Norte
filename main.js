99cda66d3e87719b3b30fec5289a57a0d9087333
marquessyan <marquessyan@gmail.com>
2025-04-22 20:22:49 -0300

    Estilização de Heiner e meus ajustes a mesma

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-let toggle = true;
+// let toggle = true;
 
-document
-  .getElementById("light-dark")
-  .addEventListener("click", function func() {
-    let dBody = document.body;
-    if (toggle === true) {
-      dBody.style.backgroundColor = "black";
-      dBody.style.color = "white";
-    } else {
-      dBody.style.color = "black";
-      dBody.style.backgroundColor = "white";
-    }
-    toggle = !toggle;
-  });
+// document
+//   .getElementById("light-dark")
+//   .addEventListener("click", function func() {
+//     let dBody = document.body;
+//     if (toggle === true) {
+//       dBody.style.backgroundColor = "black";
+//       dBody.style.color = "white";
+//     } else {
+//       dBody.style.color = "black";
+//       dBody.style.backgroundColor = "white";
+//     }
+//     toggle = !toggle;
+//   });
+
+document.getElementById("light-dark").addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
